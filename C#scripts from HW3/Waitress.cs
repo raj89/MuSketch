54e4eb2fc2dff5b329207ccc37c16ad89240303e
raj89 <rajaramanin@rajas-mbp.home>
2014-07-12 23:16:05 -0400

    Some C#scripts from HW3
    If you feel like using these/ converting them to javascript, have a
    blast!

---
+++ C#scripts from HW3/Waitress.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Waitress : MonoBehaviour {
+
+	public AudioClip [] orderSounds;
+
+	public Vector3 target1 = new Vector3(-10, 2, -25);
+	public Vector3 target2 = new Vector3(-4, 2, -15);
+	public float time = 1.0f;
+	public int waitTime = 1;
+
+IEnumerator Start () 
+{
+
+	Vector3 startPoint = transform.position;
+
+	while (true) 
+		{
+	        yield return StartCoroutine(MoveObject(transform, startPoint, target1, time, waitTime));
+	        yield return StartCoroutine(MoveObject(transform, target1, target2, time, waitTime));
+	        yield return StartCoroutine(MoveObject(transform, target2, startPoint, time, waitTime));
+	    }
+}
+
+IEnumerator MoveObject (Transform thisTransform, Vector3 startPos, Vector3 endPos, float time, int waitTime) 
+{
+
+    float fractDist = 0.0f;
+
+    float fractSpeed = 1.0f/ time;
+
+    while (fractDist < 1.0f) 
+    {
+        fractDist += Time.deltaTime * fractSpeed;
+        thisTransform.position = Vector3.Lerp(startPos, endPos, fractDist);
+        yield return null; 
+    }
+    PlayRandom();
+    yield return new WaitForSeconds(waitTime);
+}
+
+void PlayRandom()
+{
+	if(audio.isPlaying)
+		return;
+
+	audio.clip = orderSounds[Random.Range(0,orderSounds.Length)];
+	audio.Play();
+
+	//Okay, gave up on this, couldn't think of a way to pass the the current audio clip length
+
+	// float clipLength = audio.clip.length;
+	// Debug.Log(clipLength);
+	// StartCoroutine(letMeTakeMyOrder(clipLength));
+}
+
+// IEnumerator letMeTakeMyOrder(float audioLength)
+// {
+// 	float length = audioLength;
+// 	yield return WaitForSeconds(length);
+// }
+
+}
---
+++ C#scripts from HW3/playRandomOnApproach.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class playRandomOnApproach : MonoBehaviour {
+
+	public AudioClip[] randSounds;
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		InitializeAudioSource();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void InitializeAudioSource()
+	{
+
+		if (audioSource != null) //if we have already set it up
+			return;
+		
+		GameObject go = new GameObject();
+		go.transform.parent = transform;
+		go.name = "Collision-" + gameObject.name;
+		audioSource = go.AddComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.tag=="Player")
+		PlayRandom();
+	}
+
+	void PlayRandom()
+	{
+		if(audioSource.isPlaying)
+			return;
+			
+		audioSource.clip = randSounds[Random.Range(0,randSounds.Length)];
+		audioSource.Play();
+	}
+
+
+}
+
